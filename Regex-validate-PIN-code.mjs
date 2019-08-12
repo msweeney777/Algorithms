08fbd5d2aa510274bc885416e570c5e0d3f0b9ee
Matt Sweeney <msweeney777@gmail.com>
2019-08-12 12:21:31 -0400

    Modified a few algorithms

--- Regex-validate-PIN-code.mjs
+++ Regex-validate-PIN-code.mjs
@@ -1,31 +1,18 @@
-// If the passed in argument from the cli contains wxactly four or six digits
+// If the passed in argument from the cli contains exactly four or six digits
 // return true; otherwise return false
 
 let input = process.argv.slice(2).pop();
 
-//Turn the string into an integer
-//input = parseInt(input, 10);
-//console.log(input)
-
 validatePIN(input);
 
-function validatePIN (pin) {
-  let pinLength = pin.toString().length;
-  for(let i = 0; i < pin.length; i++) {
-    if(pin.charAt(i) === '.') {
-      //pin = pin.replace('.', '');
-      //console.log(pin);
-      pinLength -= 1;
+
+function validatePIN (pin) { 
+  if(pin.length === 4 || pin.length === 6) {
+    if(/^(\d{4}|\d{6})$/.test(pin)) {
+      console.log(true);
+      return true;
     }
+    console.log(false);
+    return false;
   }
-  pin = Math.abs(pin);
-  console.log(pin)
-  //input = parseInt(input, 10);
-  console.log(pinLength);
-  if(!isNaN(pin) && pinLength === 4 || pinLength === 6) {
-    console.log(true);
-    return true;
-  }
-  console.log(false);
-  return false;
 }
